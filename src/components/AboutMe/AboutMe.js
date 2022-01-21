24de11744a4b3a4117cbbf911f1b6219b82f8959
Natalya Olimpieva <ukurennyi.koshak@gmail.com>
2022-01-21 19:28:24 +0300

    style: refactor main page

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -14,7 +14,7 @@ function AboutMe() {
                 <img alt='Фото студента' src={studentImage} className="about-me__image" />
                 <h3 className="about-me__name">Мистер Робот</h3>
                 <p className="about-me__career">Фронтенд-разработчик, 30 лет</p>
-                <p className="about-me__description">Работаю в компании по кибербезопасности Allsafe Cybersecurity. Lf-lflfl дададада дададада фывФВаЙ оатдЛОУТАДОЫА ЛЫОВАЛОРЛОР лорлорлорлывлоырвалор ориориор орпорпорп Соучредитель и лидер хакерской группы FSociety. Есть сестра и воображаемый отец. Социофоб.</p>
+                <p className="about-me__description">Работаю в компании по кибербезопасности Allsafe Cybersecurity. Lf-lflfl дададада дададада фывФВаЙ оатдЛОУТАДОЫА ЛЫОВАЛОРЛОР лорлорло рлывл ориориор орпорпорп Соучредитель и лидер хакерской группы FSociety. Есть сестра и воображаемый отец. Социофоб.</p>
                 <div className="about-me__social">
                     <a
                         className="about-me__link"
