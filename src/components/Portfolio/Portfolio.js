23e84a46a44f948a16803145186eb10f2512f107
Olimpieva Natalya <ukurennyi.koshak@gmail.com>
2022-01-31 21:52:59 +0300

    style:  fix portfolio links, tech class

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -9,35 +9,35 @@ function Portfolio() {
         <div className="portfolio">
             <h3 className="portfolio__title"> Портфолио</h3>
             <ul className="portfolio__list">
-                <li className="portfolio__item">Статичный сайт
+                <li className="portfolio__item">
                     <a
                         className="portfolio__item-link"
                         href="https://github.com/Olimpieva/how-to-learn"
                         target="_blank"
                         rel="noreferrer"
-                    >
+                    >Статичный сайт
                         <img className="portfolio__logo" src={pointerIcon} alt="arrow"></img>
                     </a>
                 </li>
 
-                <li className="portfolio__item">Адаптивный сайт
+                <li className="portfolio__item">
                     <a
                         className="portfolio__item-link"
                         href="https://github.com/Olimpieva/russian-travel"
                         target="_blank"
                         rel="noreferrer"
-                    >
+                    >Адаптивный сайт
                         <img className="portfolio__logo" src={pointerIcon} alt="arrow"></img>
                     </a>
                 </li>
 
-                <li className="portfolio__item">Одностраничное приложение
+                <li className="portfolio__item">
                     <a
                         className="portfolio__item-link"
                         href="https://github.com/Olimpieva/react-mesto-api-full"
                         target="_blank"
                         rel="noreferrer"
-                    >
+                    >Одностраничное приложение
                         <img className="portfolio__logo" src={pointerIcon} alt="arrow"></img>
                     </a>
 
